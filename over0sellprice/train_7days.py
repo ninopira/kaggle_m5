a71b2fc68684eaf1a80917d235e8934639a79670
masato.ninohira <masato.ninohira@brainpad.co.jp>
2020-04-09 20:27:15 +0900

    7day(zero_dem)

--- over0sellprice/train_7days.py
+++ over0sellprice/train_7days.py
@@ -17,7 +17,7 @@ from wrmse import weight_calc
 use_top_importance = False
 num_features = 50
 
-result_dir = './result/set_seed/7day/baseline_shop_no_price_again_add_4weekdays_stat_std_shop_cumsum/'
+result_dir = './result/set_seed/7day/baseline_shop_no_price_again_add_4weekdays_stat_std_shop_cumsum_zerodem/'
 os.makedirs(result_dir, exist_ok=True)
 print(result_dir)
 
@@ -41,6 +41,7 @@ print('before_merged_shape:{}'.format(df_all.shape))
 t0_all = time.time()
 
 f_paths = [
+    './feature/zero_demand/f_id_zero_demand.pkl',
     './feature/cumsum/f_id_cumsum_demand.pkl',
     './feature/shop/f_diff_ave_sales_day_store_dept_std.pkl',
     './feature/lag_demand/f_id_lag_demand_4weekdays_stat.pkl',
