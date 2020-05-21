0dbac8a2d389bd9cce5ea0f19c0b63867920c058
masato.ninohira <masato.ninohira@brainpad.co.jp>
2020-05-21 17:37:01 +0900

    paramを変更

--- 28model/train_28.py
+++ 28model/train_28.py
@@ -20,7 +20,7 @@ extract_test_day = datetime.datetime(2016, 4, 24) + datetime.timedelta(days=days
 extract_test_day = extract_test_day.strftime('%Y-%m-%d')
 print(extract_test_day)
 
-result_dir = f'./result/28model/no_price_shop_cumsum_zerodem_dem_shop_std_week_trend_4weekstat_more_lag_rm-ch_cate/day{days}'
+result_dir = f'./result/28model/no_price_shop_cumsum_zerodem_dem_shop_std_week_trend_4weekstat_more_lag_rm-ch_cate_params/day{days}'
 os.makedirs(result_dir, exist_ok=True)
 print(result_dir)
 
@@ -194,11 +194,10 @@ params = {
     'objective': 'poisson',
     'n_jobs': -1,
     'seed': 20,
-    'learning_rate': 0.05,
-    'alpha': 0.1,
-    'lambda': 0.1,
+    'learning_rate': 0.08,
+    'num_leaves': 63,
     'bagging_fraction': 0.66,
-    'bagging_freq': 2,
+    'bagging_freq': 1,
     'colsample_bytree': 0.77}
 
 
